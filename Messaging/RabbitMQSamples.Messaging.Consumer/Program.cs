7fb8b40762e32b64a8f98e2f5a3c6b95210d7036
Ali Majidi <seyedalimajidi76@gmail.com>
2024-09-02 14:43:36 +0330

    Add project files.

---
+++ Messaging/RabbitMQSamples.Messaging.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace RabbitMQSamples.Messaging.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    string queueName = "MessagingQueue";
+                    channel.QueueDeclare(queueName, false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume(queueName, true, consumer);
+                    Console.WriteLine("Press [ENTER] to close program \n");
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body.ToArray());
+            Console.WriteLine($"[ - ] Message recevid : {message}");
+        }
+    }
+}
---
+++ Messaging/RabbitMQSamples.Messaging.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQSamples.Messaging.Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    string queueName = "MessagingQueue";
+                    channel.QueueDeclare(queueName,false,false,false,null);
+                    Console.WriteLine($"queue {queueName} was created successed!");
+                    Console.WriteLine("\nPlease write your message and Press [ENTER] : \n");
+                    string message = Console.ReadLine();
+                    byte[] messageBytes = Encoding.UTF8.GetBytes(message).ToArray();
+                    channel.BasicPublish("", queueName, null,messageBytes);
+                    Console.WriteLine("your message sent successed!");
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+    }
+}
