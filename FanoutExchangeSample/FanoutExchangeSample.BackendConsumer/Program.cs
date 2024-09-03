cb21232b020b30e2ded869c78bc3048e08652dcf
Ali Majidi <seyedalimajidi76@gmail.com>
2024-09-03 19:36:58 +0330

    fanout exchange sample

---
+++ FanoutExchangeSample/FanoutExchangeSample.BackendConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace FanoutExchangeSample.BackendConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FrontEnd Developers messaging box");
+
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    string exchangeName = "myFanoutExchange";
+                    channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, true, false, null);
+
+                    string queueName = "BackendDevelopers";
+                    channel.QueueDeclare(queueName, true, false, false, null);
+                    channel.QueueBind(queueName, exchangeName, "", null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume(queueName, true, consumer);
+                    Console.WriteLine("press any key to close app");
+                    Console.ReadKey();
+                }
+            }
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            byte[] messageBytes = e.Body.ToArray();
+            string message = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"[-] received new message : {message}");
+        }
+    }
+}
---
+++ FanoutExchangeSample/FanoutExchangeSample.Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace FanoutExchangeSample.Producer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost"};
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    string exchangeName = "myFanoutExchange";
+                    channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, true, false, null);
+
+                    while (true)
+                    {
+                        Console.WriteLine("Press key : \n[1] send new message\n[2] close application");
+                        if (Console.ReadLine() == "1")
+                        {
+                            Console.WriteLine("please write your message :");
+                            string message = Console.ReadLine();
+                            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish(exchangeName, "", null, messageBytes);
+                            Console.WriteLine("your message has been successfully sent to all groups!");
+                            Console.WriteLine("press any key to countinue");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        else break;
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
